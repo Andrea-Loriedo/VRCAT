073c66af256cc38f2a54c5714454ce8af732208b
unknown <el16al@leeds.ac.uk>
2019-09-11 15:41:37 +0100

    Implemented feedback system in the tracking task

---
+++ Assets/MyScripts/Tracking/ExternalColliderController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExternalColliderController : MonoBehaviour
+{
+    [SerializeField] TrackingFeedbackController feedback;
+
+    Status status;
+
+    bool onTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        onTarget = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckDistance();
+        feedback.ShowFeedback(status);
+    }
+
+    void OnTriggerEnter()
+    {
+        onTarget = true;
+    }
+
+    void OnTriggerExit()
+    {
+        onTarget = false;
+    }
+
+    void CheckDistance()
+    {
+        status = onTarget ? Status.Close : Status.Far;
+    }
+}
+
---
+++ Assets/MyScripts/Tracking/TrackingFeedbackController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrackingFeedbackController : MonoBehaviour
+{
+
+    [SerializeField] UXF.Session session;
+    [SerializeField] Text feedbackText;
+
+    void Awake()
+    {
+        Clear();
+    }
+
+    public void ShowFeedback(Status status)
+    {
+       switch(status)
+       {
+           case Status.Close:
+           feedbackText.text = "Perfect!";
+           break;
+           case Status.Far:
+           feedbackText.text = "Get closer to the target";
+           break;
+       }
+    }
+
+    public void Clear()
+    {
+        feedbackText.text = "";
+    }
+
+}
+
+public enum Status
+{
+    Close, Far
+}
