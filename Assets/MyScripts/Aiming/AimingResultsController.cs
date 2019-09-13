b0677b36ba562ca6bab64e0ca18c1430a30d4f62
unknown <el16al@leeds.ac.uk>
2019-09-13 14:50:51 +0100

    Implemented speed recording for cursor in aiming task

--- Assets/MyScripts/Aiming/AimingResultsController.cs
+++ Assets/MyScripts/Aiming/AimingResultsController.cs
@@ -4,41 +4,49 @@ using UnityEngine;
 
 public class AimingResultsController : MonoBehaviour
 {
+    [HideInInspector] public float speed;
     public UXF.Session session;
-
-    public void RecordVelocity(Gameobject cursor, Vector3 velocity)
+    Vector3 m_physMousePos;
+    
+    void Start()
     {
-        Vector3 averageVelocity = velocity.x
-        velocity;
+    }
+    
+    public void RecordVelocity(Vector3 currPosition, Vector3 prevPosition)
+    {
+        // speed = rb.velocity.magnitude;  
 
+        // Debug.LogFormat("Speed = {0}", speed);  
+        float speed = (currPosition - prevPosition).magnitude / Time.fixedDeltaTime;
+        Debug.LogFormat("Speed = {0}", speed);
     }
 
-    public void RecordResults(Vector3 cursorPosition, Vector3 velocity)
-    {
-        if (activeTarget)
-        {
-            Vector3 hitPos = cursorPosition;
-            hitPos.z = transform.position.z;
-            bool hit = activeTarget.CheckHit(hitPos, velocity);
-            Outcome outcome = hit ? Outcome.Hit : Outcome.Miss;
+    // public void RecordResults(Vector3 cursorPosition, Vector3 velocity)
+    // {
+    //     if (activeTarget)
+    //     {
+    //         Vector3 hitPos = cursorPosition;
+    //         hitPos.z = transform.position.z;
+    //         bool hit = activeTarget.CheckHit(hitPos, velocity);
+    //         Outcome outcome = hit ? Outcome.Hit : Outcome.Miss;
 
-            UXF.Trial trial = session.CurrentTrial;
-            trial.result["outcome"] = outcome;
-            trial.result["target_y_position"] = activeTarget.transform.localPosition.y;
-            trial.result["intercept_x_position"] = cursorPosition.x;
-            trial.result["intercept_x_velocity"] = velocity.x;
-            trial.result["intercept_z_velocity"] = velocity.z;
-            trial.result["intercept_time"] = Time.time;
+    //         UXF.Trial trial = session.CurrentTrial;
+    //         trial.result["outcome"] = outcome;
+    //         trial.result["target_y_position"] = activeTarget.transform.localPosition.y;
+    //         trial.result["intercept_x_position"] = cursorPosition.x;
+    //         trial.result["intercept_x_velocity"] = velocity.x;
+    //         trial.result["intercept_z_velocity"] = velocity.z;
+    //         trial.result["intercept_time"] = Time.time;
 
-            session.Invoke("EndCurrentTrial", trial.settings.GetFloat("post_intercept_buffer_duration"));
+    //         session.Invoke("EndCurrentTrial", trial.settings.GetFloat("post_intercept_buffer_duration"));
 
-            if (hit & trial.settings.GetString("block_type") != "Practice") score.Increment();
+    //         if (hit & trial.settings.GetString("block_type") != "Practice") score.Increment();
 
-            activeTarget = null;
+    //         activeTarget = null;
 
-            feedback.ShowFeedback(outcome);
+    //         feedback.ShowFeedback(outcome);
 
-            startZone.ClearTimeout();
-        }
-    }
+    //         startZone.ClearTimeout();
+    //     }
+    // }
 }
--- Assets/MyScripts/ModeManagement/CursorController.cs
+++ Assets/MyScripts/ModeManagement/CursorController.cs
@@ -4,7 +4,33 @@ using UnityEngine;
 
 public class CursorController : MonoBehaviour
 {
+    [SerializeField] AimingResultsController results;
+    Rigidbody rb;
     public Transform target;
+    Vector3 currPos;
+    Vector3 prevPos;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        prevPos = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        currPos = transform.position;
+        // try
+        //     {
+        //         if(rb != null)
+        //         results.RecordVelocity(rb);
+        //     }
+        //     catch (System.FormatException e)
+        //     {
+        //         ;
+        //     }
+        results.RecordVelocity(currPos, prevPos);
+        prevPos = currPos;
+    }
 
     void Update()
     {
